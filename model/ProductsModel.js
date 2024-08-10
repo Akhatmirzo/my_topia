5ee36958bf20f5f7e9b5b9561a55beaa4164d2a1
Akhatmirzo <blackprincepbg@gmail.com>
2024-08-10 22:20:19 +0500

    some change

--- model/ProductsModel.js
+++ model/ProductsModel.js
@@ -5,10 +5,6 @@ const ProductSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  description: {
-    type: String,
-    required: true,
-  },
   price: {
     type: Number,
     required: true,
